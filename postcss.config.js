b1275691691fcf7690989aab6cdb9348d412f31d
Evgeniy Talko <etalko@gmail.com>
2021-04-25 11:40:00 +0300

    add(postcss-plugins): url, sorting, import

--- postcss.config.js
+++ postcss.config.js
@@ -1,8 +1,11 @@
 module.exports = {
   plugins: [
+    require("postcss-url"),
+    require("postcss-sorting"),
     require("postcss-preset-env"),
     require("tailwindcss"),
     require("stylelint"),
     require("autoprefixer"),
+    require("postcss-import"),
   ],
 };
